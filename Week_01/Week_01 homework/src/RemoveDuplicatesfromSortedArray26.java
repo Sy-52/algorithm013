ffe004989f306ab5d4ca5df953739f1e0637d589
Yikai Fan Desktop <fykwhl@163.com>
2020-08-04 13:17:53 +0800

    重新更改了一下顺序，采用自顶向下编程方式

--- Week_01/Week_01 homework/src/RemoveDuplicatesfromSortedArray26.java
+++ Week_01/Week_01 homework/src/RemoveDuplicatesfromSortedArray26.java
@@ -1,4 +1,11 @@
 public class RemoveDuplicatesfromSortedArray26 {
+    public static void main(String[] args) {
+        int[] test = {0,0,1,1,1,2,2,3,3,4};
+        int length = removeDuplicates(test);
+        for (int i = 0; i < length; i++) {
+            System.out.println(test[i]);
+        }
+    }
     //方法一：
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
@@ -11,14 +18,6 @@ public class RemoveDuplicatesfromSortedArray26 {
         }
         return i + 1;
     }
-    //方法二：
-
-    public static void main(String[] args) {
-        int[] test = {0,0,1,1,1,2,2,3,3,4};
-        int length = removeDuplicates(test);
-        for (int i = 0; i < length; i++) {
-            System.out.println(test[i]);
-        }
-    }
+    //方法二:
 }
 
--- Week_01/Week_01 homework/src/RotateArray189.java
+++ Week_01/Week_01 homework/src/RotateArray189.java
@@ -1,4 +1,11 @@
 public class RotateArray189 {
+    public static void main(String[] args) {
+        int[] test = {1,2,3,4,5,6,7};
+        rotate(test,3);
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
+    }
     //方法一：
     public static void rotate(int[] nums, int k) {
         int temp, previous;
@@ -12,13 +19,5 @@ public class RotateArray189 {
         }
     }
     //方法二：
-    
-    public static void main(String[] args) {
-        int[] test = {1,2,3,4,5,6,7};
-        rotate(test,3);
-        for (int i = 0; i < test.length; i++) {
-            System.out.println(test[i]);
-        }
-    }
 }
 
